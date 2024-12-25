65b3e290b176a89bb705f2b1d5f26463e5b87ff0
eestdnt <hmink143@gmail.com>
2024-12-25 22:35:53 +0200

    Add random number output for DAQmxReadAnalogF64

--- src/nidaqmx.c
+++ src/nidaqmx.c
@@ -1,82 +1,89 @@
-#include <string.h>
 #include "nidaqmx.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
-int32 DAQmxGetErrorString (int32 errorCode, char errorString[], uInt32 bufferSize)
+int32 DAQmxGetErrorString(int32 errorCode, char errorString[],
+                          uInt32 bufferSize)
 {
-    return 0;
+  return 0;
 }
 
-int32 DAQmxCreateTask (const char taskName[], TaskHandle *taskHandle)
+int32 DAQmxCreateTask(const char taskName[], TaskHandle *taskHandle)
 {
-    return 0;
+  return 0;
 }
 
-int32 DAQmxClearTask (TaskHandle taskHandle)
+int32 DAQmxClearTask(TaskHandle taskHandle) { return 0; }
+
+int32 DAQmxCreateAIVoltageChan(TaskHandle taskHandle,
+                               const char physicalChannel[],
+                               const char nameToAssignToChannel[],
+                               int32 terminalConfig, float64 minVal,
+                               float64 maxVal, int32 units,
+                               const char customScaleName[])
 {
-    return 0;
+  return 0;
 }
 
-int32 DAQmxCreateAIVoltageChan (TaskHandle taskHandle, const char physicalChannel[], const char nameToAssignToChannel[], int32 terminalConfig, float64 minVal, float64 maxVal, int32 units, const char customScaleName[])
+int32 DAQmxGetDevAIVoltageRngs(const char device[], float64 *data,
+                               uInt32 arraySizeInSamples)
 {
-    return 0;
+  return 0;
 }
 
-int32 DAQmxGetDevAIVoltageRngs(const char device[], float64 *data, uInt32 arraySizeInSamples)
+int32 DAQmxCreateAOVoltageChan(TaskHandle taskHandle,
+                               const char physicalChannel[],
+                               const char nameToAssignToChannel[],
+                               float64 minVal, float64 maxVal, int32 units,
+                               const char customScaleName[])
 {
-    return 0;
+  return 0;
 }
 
-int32 DAQmxCreateAOVoltageChan (TaskHandle taskHandle, const char physicalChannel[], const char nameToAssignToChannel[], float64 minVal, float64 maxVal, int32 units, const char customScaleName[])
+int32 DAQmxCfgSampClkTiming(TaskHandle taskHandle, const char source[],
+                            float64 rate, int32 activeEdge, int32 sampleMode,
+                            uInt64 sampsPerChanToAcquire)
 {
-    return 0;
+  return 0;
 }
 
-int32 DAQmxCfgSampClkTiming (TaskHandle taskHandle, const char source[], float64 rate, int32 activeEdge, int32 sampleMode, uInt64 sampsPerChanToAcquire)
+int32 DAQmxCfgDigEdgeStartTrig(TaskHandle taskHandle,
+                               const char triggerSource[], int32 triggerEdge)
 {
-    return 0;
+  return 0;
 }
 
-int32 DAQmxCfgDigEdgeStartTrig (TaskHandle taskHandle, const char triggerSource[], int32 triggerEdge)
+int32 DAQmxWriteAnalogF64(TaskHandle taskHandle, int32 numSampsPerChan,
+                          bool32 autoStart, float64 timeout, bool32 dataLayout,
+                          const float64 writeArray[],
+                          int32 *sampsPerChanWritten, bool32 *reserved)
 {
-    return 0;
+  return 0;
 }
 
-int32 DAQmxWriteAnalogF64 (TaskHandle taskHandle, int32 numSampsPerChan, bool32 autoStart, float64 timeout, bool32 dataLayout, const float64 writeArray[], int32 *sampsPerChanWritten, bool32 *reserved)
-{
-    return 0;
-}
+int32 DAQmxStartTask(TaskHandle taskHandle) { return 0; }
 
-int32 DAQmxStartTask (TaskHandle taskHandle)
-{
-    return 0;
-}
-
-int32 DAQmxStopTask (TaskHandle taskHandle)
-{
-    return 0;
-}
+int32 DAQmxStopTask(TaskHandle taskHandle) { return 0; }
 
 int32 DAQmxGetTaskNumChans(TaskHandle taskHandle, uInt32 *data)
 {
-    *data = 2;
-    return 0;
+  *data = 2;
+  return 0;
 }
 
-int32 DAQmxResetDevice(const char deviceName[])
-{
-    return 0;
-}
+int32 DAQmxResetDevice(const char deviceName[]) { return 0; }
 
-int32 DAQmxReadAnalogF64 (TaskHandle taskHandle, int32 numSampsPerChan, float64 timeout, bool32 fillMode, float64 readArray[], uInt32 arraySizeInSamps, int32 *sampsPerChanRead, bool32 *reserved)
+int32 DAQmxReadAnalogF64(TaskHandle taskHandle, int32 numSampsPerChan,
+                         float64 timeout, bool32 fillMode, float64 readArray[],
+                         uInt32 arraySizeInSamps, int32 *sampsPerChanRead,
+                         bool32 *reserved)
 {
-    if (numSampsPerChan < 0)
-    {
-        *sampsPerChanRead = arraySizeInSamps/2;
-    }
-    else
-    {
-        *sampsPerChanRead = numSampsPerChan;
-    }
-    memset(readArray, 0, arraySizeInSamps * sizeof(float64));
-    return 0;
+  if (numSampsPerChan < 0) {
+    *sampsPerChanRead = arraySizeInSamps / 2;
+  } else {
+    *sampsPerChanRead = numSampsPerChan;
+  }
+  memset(readArray, (uint8_t)(rand() % 256), arraySizeInSamps * sizeof(float64));
+  return 0;
 }
